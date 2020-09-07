2fb12e82af724a32b5ee3e7f0cfeceefbcaf92d7
JY <jingyuzhang2008@gmail.com>
2020-09-07 21:57:16 +1000

    Add a test case

--- tests/test_data_profiling.py
+++ tests/test_data_profiling.py
@@ -1,24 +1,7 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import pytest
-from unittest import TestCase
 import pandas as pd
-
-from data_profilling.data_profiling import Profiling
-
-
-class TryTesting(TestCase):
-    def test_always_passes(self):
-        self.assertTrue(True)
 
 
 def test_summary():
     iris = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
-    print(1111)
-    print(Profiling.summary(iris))
-    assert iris == 2, "test failed"
-    # assert fib(2) == 1
-    # assert fib(7) == 13
-    # with pytest.raises(AssertionError):
-    #     fib(-10)
+
+    assert iris.shape[0] == 150
